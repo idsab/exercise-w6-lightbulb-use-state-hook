face9c014f0da244f0250b6b8a85454ad4132bd3
idsab <idasabic@gmail.com>
2024-03-12 14:50:32 +0100

    week 6 team practice useState
    Co-authored-by: Lovisaaberg <Lovisaaberg@users.noreply.github.com>
    Co-authored-by: fridaforser <fridaforser@users.noreply.github.com>
    Co-authored-by: Pernilla Sterner <hey@pernillasterner.se>
    Co-authored-by: Erika Wernbro <Tejpex@users.noreply.github.com>
    Co-authored-by: Alexandra Franzén <Xikitita@users.noreply.github.com>
    Co-authored-by: AHPIXI <AHPIXI@users.noreply.github.com>

--- src/components/Lightbulb.jsx
+++ src/components/Lightbulb.jsx
@@ -1,15 +1,45 @@
+// Import useState
+import { useState } from "react";
+// let color = "on";
+
 export const Lightbulb = () => {
   // Initialize the state for on/off
+  const [theme, setTheme] = useState(true);
+  //const [color, setColor] = useState("on");
 
+  
   const toggleLight = () => {
     // Toggle the on/off state
-    console.log("Clicked");
+    //alternative 1
+    /*if (theme === "on") {
+      setTheme("crazy");
+    } else if (theme === "crazy") {
+      setTheme("off");
+    } else {
+      setTheme("on");
+    }*/
+    
+//alternative 2
+    /*if (theme) {
+      setTheme(false);
+      setColor("off")
+    } else {
+      setTheme(true);
+      setColor("on")
+    }*/
+
+    // Ternary operator alternative 3
+    setTheme(theme === "on" ? "off" :  "on" )
+    //setTheme(theme ? false : true);
   };
+  //console.log(theme);
+
   return (
     <div className="lightbulb-container">
-      <div className="lightbulb" onClick={toggleLight}>
+        <div className={`lightbulb ${theme}`} onClick={toggleLight}>
         <div className="bulb"></div>
       </div>
+      <p>{theme}</p>
     </div>
   );
 };
